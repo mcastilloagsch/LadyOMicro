8da9a04dcd24041313e3eb177ed47b2bd4ede709
julian_ortega_silva <practicante_ti2@guiasyscoutschile.cl>
2022-09-29 19:41:08 -0300

    202209291942 - GendersAPI con CRUD Inicial.

--- LadyO.API/Models/Genders.cs
+++ LadyO.API/Models/Genders.cs
@@ -54,27 +54,33 @@ namespace LadyO.API.Models
             }
         }
 
+        private static Genders getObj(int id)
+        {
+            List<Genders> objReturnList = new List<Genders>();
+            string sqlQuery = "SELECT id, name FROM " + Generic.DBConnection.SCHEMA + ".genders WHERE id = " + id;
+            using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
+            {
+                using (MySqlCommand comando = new MySqlCommand(sqlQuery, conexion))
+                {
+                    conexion.Open();
+                    MySqlDataReader reader = comando.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        objReturnList.Add(new Genders(reader.GetInt32(0), reader.GetString(1)));
+                    }
+                    conexion.Close();
+                }
+            }
+            return objReturnList.FirstOrDefault();
+        }
+
         public static object getObject(int id)
         {
             APIGenericResponse response = new APIGenericResponse();
             try
             {
-                List<Genders> objReturnList = new List<Genders>();
-                string sqlQuery = "SELECT id, name FROM " + Generic.DBConnection.SCHEMA + ".genders WHERE id = " + id;
-                using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
-                {
-                    using (MySqlCommand comando = new MySqlCommand(sqlQuery, conexion))
-                    {
-                        conexion.Open();
-                        MySqlDataReader reader = comando.ExecuteReader();
-                        while (reader.Read())
-                        {
-                            objReturnList.Add(new Genders(reader.GetInt32(0), reader.GetString(1)));
-                        }
-                        conexion.Close();
-                    }
-                }
-                if (objReturnList.FirstOrDefault() == null)
+                Genders objReturn = Genders.getObj(id);
+                if (objReturn == null)
                 {
                     response.isValid = false;
                     response.msg = Generic.Message.ID_GENDERS_GETOBJECT_NO_EXISTE;
@@ -85,7 +91,7 @@ namespace LadyO.API.Models
                 {
                     response.isValid = true;
                     response.msg = string.Empty;
-                    response.data = objReturnList.FirstOrDefault();
+                    response.data = objReturn;
                     return response;
                 }
             }
@@ -97,76 +103,39 @@ namespace LadyO.API.Models
                 return response;
             }
         }
-        private static Genders getGender(int id)
-        {
-            try
-            {
-                List<Genders> objReturnList = new List<Genders>();
-                string sqlQuery = "SELECT id, name FROM " + Generic.DBConnection.SCHEMA + ".genders WHERE id = " + id;
-                using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
-                {
-                    using (MySqlCommand comando = new MySqlCommand(sqlQuery, conexion))
-                    {
-                        conexion.Open();
-                        MySqlDataReader reader = comando.ExecuteReader();
-                        while (reader.Read())
-                        {
-                            objReturnList.Add(new Genders(reader.GetInt32(0), reader.GetString(1)));
-                        }
-                        conexion.Close();
-                    }
-                }
-                if (objReturnList.FirstOrDefault() != null)
-                {
-                    return objReturnList.FirstOrDefault();
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
+        
 
 
-        public static object ObjInsert(Genders objInsert)
+        public static object objAdd(Genders obj)
         {
             APIGenericResponse response = new APIGenericResponse();
-            Genders objData = new Genders();
+            response.data = null;
             try
             {
-                string str_name = objInsert.name;
-                string String_name = Regex.Replace(str_name, @"\s", "");
-                int length_name = String_name.Length;
-                if (length_name >= 1)
+                if (obj.name.Length > 0)
                 {
-                    string sqlQuery = "INSERT INTO " + Generic.DBConnection.SCHEMA + ".genders VALUES(0, '" + objInsert.name + "');SELECT LAST_INSERT_ID();";
+                    string sqlQuery = "INSERT INTO " + Generic.DBConnection.SCHEMA + ".genders VALUES(0, '" + Generic.Tools.Capital(obj.name) + "');SELECT LAST_INSERT_ID();";
                     using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
 
                     {
                         using (MySqlCommand comando = new MySqlCommand(sqlQuery, conexion))
                         {
                             conexion.Open();
-                            objData.id = Convert.ToInt32(comando.ExecuteScalar());
+                            obj.id = Convert.ToInt32(comando.ExecuteScalar());
                             conexion.Close();
                         }
                     }
-                    objData.name = objInsert.name;
                     response.isValid = true;
                     response.msg = string.Empty;
-                    response.data = objData;
-                    return response;
+                    response.data = obj;
                 }
                 else
                 {
                     response.isValid = false;
                     response.msg = Generic.Message.NAME_NO_EXISTE;
-                    response.data = null;
                     return response;
                 }
+                return response;
             }
             catch (Exception ex)
             {
@@ -178,52 +147,55 @@ namespace LadyO.API.Models
         }
 
 
-        public static object ObjUpdate(Genders objUpdate)
+        public static object objUpdate(Genders obj)
         {
             APIGenericResponse response = new APIGenericResponse();
-            Genders objData = new Genders();
+            response.data = null;
             try
             {
-                string str_name = objUpdate.name;
-                string String_name = Regex.Replace(str_name, @"\s", "");
-                int length_name = String_name.Length;
-                if (length_name >= 1)
+                if (obj.id > 0)
                 {
-                    Genders valid = getGender(objUpdate.id);
-                    if (valid != null)
+                    Genders objUpdate = new Genders();
+                    objUpdate = Genders.getObj(obj.id);
+                    if (objUpdate != null)
                     {
-                        string sqlQueryUpdate = "UPDATE " + Generic.DBConnection.SCHEMA + ".genders SET name = '" + objUpdate.name + "'  WHERE id =  " + objUpdate.id;
-                        using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
+                        if(obj.name.Length > 0)
                         {
-                            using (MySqlCommand comando = new MySqlCommand(sqlQueryUpdate, conexion))
+                            string sqlQueryUpdate = "UPDATE " + Generic.DBConnection.SCHEMA + ".genders SET name = '" + Generic.Tools.Capital(obj.name) + "'  WHERE id =  " + obj.id;
+                            using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
                             {
-                                conexion.Open();
-                                comando.ExecuteReader();
-                                conexion.Close();
+                                using (MySqlCommand comando = new MySqlCommand(sqlQueryUpdate, conexion))
+                                {
+                                    conexion.Open();
+                                    comando.ExecuteReader();
+                                    conexion.Close();
+                                }
                             }
+                            response.isValid = true;
+                            response.msg = string.Empty;
+                            response.data = Genders.getObj(obj.id);
                         }
-                        objData.id = objUpdate.id;
-                        objData.name = objUpdate.name;
-                        response.isValid = true;
-                        response.msg = string.Empty;
-                        response.data = objData;
-                        return response;
+                        else
+                        {
+                            response.isValid = false;
+                            response.msg = Generic.Message.NAME_NO_EXISTE;
+                            return response;
+                        }
                     }
                     else
                     {
                         response.isValid = false;
                         response.msg = Generic.Message.ID_GENDERS_NO_EXISTE;
-                        response.data = null;
                         return response;
                     }
                 }
                 else
                 {
                     response.isValid = false;
-                    response.msg = Generic.Message.NAME_NO_EXISTE;
-                    response.data = null;
+                    response.msg = Generic.Message.ID_GENDERS_NO_EXISTE;
                     return response;
                 }
+                return response;
             }
             catch (Exception ex)
             {
@@ -233,7 +205,5 @@ namespace LadyO.API.Models
                 return response;
             }
         }
-
-
     }
 }
