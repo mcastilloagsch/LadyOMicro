cc9dfcfc45e990a6e6152891220c5cd4903415fd
julian_ortega_silva <practicante_ti2@guiasyscoutschile.cl>
2022-10-18 23:36:31 -0300

    202210182338 - Cambio a Base de Datos Final - API Gender completada y lista para QA.

---
+++ LadyO.API/Controllers/GenderController.cs
@@ -0,0 +1,152 @@
+﻿using LadyO.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LadyO.API.Controllers
+{
+    public class GenderController : ApiController
+    {
+        [Route("api/Gender/getObject/{idGender}")]
+        [HttpGet]
+        public object getObject(int idGender)
+        {
+            try
+            {
+                return Models.Gender.getObject(idGender);
+            }
+            catch (Exception ex)
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Gender/objAdd")]
+        [HttpPost]
+        public object objAdd([FromBody] Models.Gender obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            try
+            {
+                object objReturn = new object();
+                if (ModelState.IsValid)
+                {
+                    return Models.Gender.objAdd(obj);
+                }
+                else
+                {
+                    response.isValid = false;
+                    response.msg = Generic.Message.OBJETO_NO_CORRESPONDE;
+                    response.data = null;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Gender/objUpdate")]
+        [HttpPut]
+        public object objUpdate([FromBody] Models.Gender obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return Models.Gender.objUpdate(obj);
+                }
+                else
+                {
+                    response.isValid = false;
+                    response.msg = Generic.Message.OBJETO_NO_CORRESPONDE;
+                    response.data = null;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Gender/objDelete")]
+        [HttpDelete]
+        public object objDelete([FromBody] Models.Gender obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return Models.Gender.objDelete(obj);
+                }
+                else
+                {
+                    response.isValid = false;
+                    response.msg = Generic.Message.OBJETO_NO_CORRESPONDE;
+                    response.data = null;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Gender/getList")]
+        [HttpGet]
+        public object getList()
+        {
+            try
+            {
+                return Models.Gender.getList(); ;
+            }
+            catch (Exception ex)
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Gender/getListAdm/{idPerson}")]
+        [HttpGet]
+        public object getListAdm(int idPerson)
+        {
+            try
+            {
+                return Models.Gender.getListAdm(idPerson);
+            }
+            catch (Exception ex)
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+    }
+}
