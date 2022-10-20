5115b83463f46c05ad8e21aed6c6b4f606f7a188
julian_ortega_silva <practicante_ti2@guiasyscoutschile.cl>
2022-10-19 23:11:54 -0300

    202210192313 - Cambio a Base de Datos Final - API Position completada y lista para QA.

---
+++ LadyO.API/Controllers/PositionController.cs
@@ -0,0 +1,152 @@
+﻿using LadyO.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LadyO.API.Controllers
+{
+    public class PositionController : ApiController
+    {
+        [Route("api/Position/getObject/{idPosition}")]
+        [HttpGet]
+        public object getObject(int idPosition)
+        {
+            try
+            {
+                return Models.Position.getObject(idPosition);
+            }
+            catch (Exception ex)
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Position/objAdd")]
+        [HttpPost]
+        public object objAdd([FromBody] Models.Position obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            try
+            {
+                object objReturn = new object();
+                if (ModelState.IsValid)
+                {
+                    return Models.Position.objAdd(obj);
+                }
+                else
+                {
+                    response.isValid = false;
+                    response.msg = Generic.Message.OBJETO_NO_CORRESPONDE;
+                    response.data = null;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Position/objUpdate")]
+        [HttpPut]
+        public object objUpdate([FromBody] Models.Position obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return Models.Position.objUpdate(obj);
+                }
+                else
+                {
+                    response.isValid = false;
+                    response.msg = Generic.Message.OBJETO_NO_CORRESPONDE;
+                    response.data = null;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Position/objDelete")]
+        [HttpDelete]
+        public object objDelete([FromBody] Models.Position obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return Models.Position.objDelete(obj);
+                }
+                else
+                {
+                    response.isValid = false;
+                    response.msg = Generic.Message.OBJETO_NO_CORRESPONDE;
+                    response.data = null;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Position/getList")]
+        [HttpGet]
+        public object getList()
+        {
+            try
+            {
+                return Models.Position.getList(); ;
+            }
+            catch (Exception ex)
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/Position/getListAdm/{idPerson}")]
+        [HttpGet]
+        public object getListAdm(int idPerson)
+        {
+            try
+            {
+                return Models.Position.getListAdm(idPerson); ;
+            }
+            catch (Exception ex)
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+    }
+}
