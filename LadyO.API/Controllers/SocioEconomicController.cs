c57ad7df8e81a6cb6267e1fd1be5e79db1a9eec7
julian_ortega_silva <practicante_ti2@guiasyscoutschile.cl>
2022-10-19 22:41:19 -0300

    202210192242 - Cambio a Base de Datos Final - API SocioEconomic completada y lista para QA.

---
+++ LadyO.API/Controllers/SocioEconomicController.cs
@@ -0,0 +1,152 @@
+﻿using LadyO.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LadyO.API.Controllers
+{
+    public class SocioEconomicController : ApiController
+    {
+        [Route("api/SocioEconomic/getObject/{idSocioEconomic}")]
+        [HttpGet]
+        public object getObject(int idSocioEconomic)
+        {
+            try
+            {
+                return Models.SocioEconomic.getObject(idSocioEconomic);
+            }
+            catch (Exception ex)
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/SocioEconomic/objAdd")]
+        [HttpPost]
+        public object objAdd([FromBody] Models.SocioEconomic obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            try
+            {
+                object objReturn = new object();
+                if (ModelState.IsValid)
+                {
+                    return Models.SocioEconomic.objAdd(obj);
+                }
+                else
+                {
+                    response.isValid = false;
+                    response.msg = Generic.Message.OBJETO_NO_CORRESPONDE;
+                    response.data = null;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/SocioEconomic/objUpdate")]
+        [HttpPut]
+        public object objUpdate([FromBody] Models.SocioEconomic obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return Models.SocioEconomic.objUpdate(obj);
+                }
+                else
+                {
+                    response.isValid = false;
+                    response.msg = Generic.Message.OBJETO_NO_CORRESPONDE;
+                    response.data = null;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/SocioEconomic/objDelete")]
+        [HttpDelete]
+        public object objDelete([FromBody] Models.SocioEconomic obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return Models.SocioEconomic.objDelete(obj);
+                }
+                else
+                {
+                    response.isValid = false;
+                    response.msg = Generic.Message.OBJETO_NO_CORRESPONDE;
+                    response.data = null;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/SocioEconomic/getList")]
+        [HttpGet]
+        public object getList()
+        {
+            try
+            {
+                return Models.SocioEconomic.getList(); ;
+            }
+            catch (Exception ex)
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+
+        [Route("api/SocioEconomic/getListAdm/{idPerson}")]
+        [HttpGet]
+        public object getListAdm(int idPerson)
+        {
+            try
+            {
+                return Models.SocioEconomic.getListAdm(idPerson);
+            }
+            catch (Exception ex)
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                response.isValid = false;
+                response.msg = ex.Message;
+                response.data = null;
+                return response;
+            }
+        }
+    }
+}
---
+++ LadyO.API/Models/SocioEconomic.cs
@@ -0,0 +1,283 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LadyO.API.Models
+{
+    public class SocioEconomic
+    {
+        public int IdSocioEconomic { get; set; }
+        public string SocioEconomicName { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public SocioEconomic()
+        {
+
+        }
+
+        public SocioEconomic(int idSocioEconomic, string socioEconomicName, bool isDeleted)
+        {
+            IdSocioEconomic = idSocioEconomic;
+            SocioEconomicName = socioEconomicName;
+            IsDeleted = isDeleted;
+        }
+
+        private static SocioEconomic getObj(int idSocioEconomic)
+        {
+            List<SocioEconomic> objReturnList = new List<SocioEconomic>();
+            string sqlQuery = "SELECT IdSocioEconomic, SocioEconomicName, IsDeleted FROM " + nameof(SocioEconomic).ToUpper() + " WHERE IdSocioEconomic = " + idSocioEconomic + ";";
+            using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
+            {
+                using (MySqlCommand comando = new MySqlCommand(sqlQuery, conexion))
+                {
+                    conexion.Open();
+                    MySqlDataReader reader = comando.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        objReturnList.Add(new SocioEconomic(reader.GetInt32(0), reader.GetString(1), reader.GetString(2) == "0" ? false : true));
+                    }
+                    conexion.Close();
+                }
+            }
+            return objReturnList.FirstOrDefault();
+        }
+
+        public static object getObject(int id)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            response.isValid = false;
+            response.data = null;
+            try
+            {
+                SocioEconomic objReturn = SocioEconomic.getObj(id);
+                if (objReturn == null)
+                {
+                    response.msg = Generic.Message.ID_SOCIOECONOMICS_GETOBJECT_NO_EXISTE;
+                    return response;
+                }
+                else
+                {
+                    response.isValid = true;
+                    response.msg = string.Empty;
+                    response.data = objReturn;
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.msg = ex.Message;
+                return response;
+            }
+        }
+
+        public static object objAdd(SocioEconomic obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            response.data = null;
+            response.isValid = false;
+            try
+            {
+                if (obj.SocioEconomicName.Length > 0)
+                {
+                    obj.SocioEconomicName = Generic.Tools.Capital(obj.SocioEconomicName);
+                    string sqlQuery = "INSERT INTO " + nameof(SocioEconomic).ToUpper() + " VALUES(NULL, '" + obj.SocioEconomicName + "', 0); SELECT LAST_INSERT_ID();";
+                    using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
+                    {
+                        using (MySqlCommand comando = new MySqlCommand(sqlQuery, conexion))
+                        {
+                            conexion.Open();
+                            obj.IdSocioEconomic = Convert.ToInt32(comando.ExecuteScalar());
+                            obj.IsDeleted = false;
+                            conexion.Close();
+                        }
+                    }
+                    response.isValid = true;
+                    response.msg = string.Empty;
+                    response.data = obj;
+                }
+                else
+                {
+                    response.msg = Generic.Message.NAME_NO_EXISTE;
+                    return response;
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.msg = ex.Message;
+                return response;
+            }
+        }
+
+        public static object objUpdate(SocioEconomic obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            response.data = null;
+            response.isValid = false;
+            try
+            {
+                if (obj.IdSocioEconomic > 0)
+                {
+                    if (SocioEconomic.getObj(obj.IdSocioEconomic) != null)
+                    {
+                        if (obj.SocioEconomicName.Length > 0)
+                        {
+                            obj.SocioEconomicName = Generic.Tools.Capital(obj.SocioEconomicName);
+                            string sqlQueryUpdate = "UPDATE " + nameof(SocioEconomic).ToUpper() + " SET SocioEconomicName = '" + obj.SocioEconomicName + "' WHERE IdSocioEconomic =  " + obj.IdSocioEconomic + ";";
+                            using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
+                            {
+                                using (MySqlCommand comando = new MySqlCommand(sqlQueryUpdate, conexion))
+                                {
+                                    conexion.Open();
+                                    comando.ExecuteReader();
+                                    conexion.Close();
+                                }
+                            }
+                            response.isValid = true;
+                            response.msg = string.Empty;
+                            response.data = obj;
+                        }
+                        else
+                        {
+                            response.msg = Generic.Message.NAME_NO_EXISTE;
+                            return response;
+                        }
+                    }
+                    else
+                    {
+                        response.msg = Generic.Message.ID_SOCIOECONOMICS_NO_EXISTE;
+                        return response;
+                    }
+                }
+                else
+                {
+                    response.msg = Generic.Message.ID_SOCIOECONOMICS_NO_EXISTE;
+                    return response;
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.msg = ex.Message;
+                return response;
+            }
+        }
+
+        public static object objDelete(SocioEconomic obj)
+        {
+            APIGenericResponse response = new APIGenericResponse();
+            response.data = null;
+            response.isValid = false;
+            try
+            {
+                if (obj.IdSocioEconomic > 0)
+                {
+                    if (SocioEconomic.getObj(obj.IdSocioEconomic) != null)
+                    {
+                        string sqlQueryUpdate = string.Empty;
+                        if (SocioEconomic.getObj(obj.IdSocioEconomic).IsDeleted)
+                        {
+                            sqlQueryUpdate = "UPDATE " + nameof(SocioEconomic).ToUpper() + " SET IsDeleted = 0 WHERE IdSocioEconomic =  " + obj.IdSocioEconomic + ";";
+                        }
+                        else
+                        {
+                            sqlQueryUpdate = "UPDATE " + nameof(SocioEconomic).ToUpper() + " SET IsDeleted = 1 WHERE IdSocioEconomic =  " + obj.IdSocioEconomic + ";";
+                        }
+                        using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
+                        {
+                            using (MySqlCommand comando = new MySqlCommand(sqlQueryUpdate, conexion))
+                            {
+                                conexion.Open();
+                                comando.ExecuteReader();
+                                conexion.Close();
+                            }
+                        }
+                        response.isValid = true;
+                        response.msg = string.Empty;
+                        response.data = SocioEconomic.getObj(obj.IdSocioEconomic);
+                    }
+                    else
+                    {
+                        response.msg = Generic.Message.ID_SOCIOECONOMICS_NO_EXISTE;
+                        return response;
+                    }
+                }
+                else
+                {
+                    response.msg = Generic.Message.ID_SOCIOECONOMICS_NO_EXISTE;
+                    return response;
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.msg = ex.Message;
+                return response;
+            }
+        }
+
+        public static object getList()
+        {
+            try
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                List<SocioEconomic> objReturnList = new List<SocioEconomic>();
+                string sqlQuery = "SELECT IdSocioEconomic, SocioEconomicName, IsDeleted FROM " + nameof(SocioEconomic).ToUpper() + " WHERE IsDeleted = 0 ORDER BY SocioEconomicName;";
+                using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
+                {
+                    using (MySqlCommand comando = new MySqlCommand(sqlQuery, conexion))
+                    {
+                        conexion.Open();
+                        MySqlDataReader reader = comando.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            objReturnList.Add(new SocioEconomic(reader.GetInt32(0), reader.GetString(1), reader.GetString(2) == "0" ? false : true));
+                        }
+                        conexion.Close();
+                    }
+                }
+                response.isValid = true;
+                response.msg = string.Empty;
+                response.data = objReturnList;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static object getListAdm(int idPerson)
+        {
+            try
+            {
+                APIGenericResponse response = new APIGenericResponse();
+                List<SocioEconomic> objReturnList = new List<SocioEconomic>();
+                string sqlQuery = "SELECT IdSocioEconomic, SocioEconomicName, IsDeleted FROM " + nameof(SocioEconomic).ToUpper() + " ORDER BY SocioEconomicName;";
+                using (MySqlConnection conexion = Generic.DBConnection.MySqlConnectionObj())
+                {
+                    using (MySqlCommand comando = new MySqlCommand(sqlQuery, conexion))
+                    {
+                        conexion.Open();
+                        MySqlDataReader reader = comando.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            objReturnList.Add(new SocioEconomic(reader.GetInt32(0), reader.GetString(1), reader.GetString(2) == "0" ? false : true));
+                        }
+                        conexion.Close();
+                    }
+                }
+                response.isValid = true;
+                response.msg = string.Empty;
+                response.data = objReturnList;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
